676b7dc38dfc5144d1be609462ae612386249919
nickfarrow <nick@nickfarrow.com>
2021-05-25 23:41:05 +1000

    Working docker

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
-# SatSale needs to connect to your bitcoin/lnd node,
-# with the correct RPC port(s) as set in this config.
+import os
 
-# Connecting through local host (see below if connecting to remote node):
-host = "127.0.0.1"
-rpcport = "8332"
+# Bitcoin node connection settings
+# This should point to your bitcoin/lnd node,
+# with the correct RPC port as set in your config.
+# Connecting through local host as  i'm running SatSale on my node
+host = os.getenv("BITCOIND_HOST")
+rpcport = os.getenv("BITCOIND_PORT")
 
 # From ~/.bitcoin/bitcoin.conf
-username = "bitcoinrpc"
-password = "rpcpassword"
+username = os.getenv("BITCOIND_USER")
+password = os.getenv("BITCOIND_PASS")
 
-# Wallet (empty "" if your node has a single wallet, OR wallet name/path as shown in `biitcoin-cli listwallets`)
+# Wallet ("" if single-wallet node, OR wallet name/path as shown in `biitcoin-cli listwallets`)
 wallet = ""
 
 # File in which API key will be stored
@@ -23,27 +25,13 @@ api_key_path = "SatSale_API_key"
 # SSH tunnel to node
 # Make sure this command works `ssh HOST@IP -q -N -L 8332:localhost:8332`
 # Use host = "127.0.0.1" and you will be able to see your node on 8332
-tunnel_host = None  # "HOST@IP"
+tunnel_host = None # "HOST@IP"
 
 # or tor hidden service for RPC (see docs for how to set up), need onion:
 tor_bitcoinrpc_host = None # e.g. "http://if...dwr.onion"
 # and a tor proxy, default 127.0.0.1:9050 (for Tor Browser use "127.0.0.1:9150")
 tor_proxy = None
-
-#### Payment method ####
-pay_method = "bitcoind"
-
-#### Lightning ####
-# Switch payment_method to lnd if you want to use lightning payments instead. And uncomment lnd_dir.
-#pay_method = "lnd"
-
-# To find (or copy from remote nodes) the macaroon and TLS certs
-#lnd_dir = "~/.lnd/"
-
-# lnd RPC port and lnd macaroon (uncomment)
-#lnd_rpcport = "10009"
-#lnd_macaroon = "invoice.macaroon"
-
+################################
 
 # Check for payment every xx seconds
 pollrate = 15
@@ -59,6 +47,16 @@ connection_attempts = 3
 
 # Generic redirect url after payment
 redirect = "https://github.com/nickfarrow/satsale"
+
+# Payment method
+pay_method = "bitcoind"
+# Switch payment_method to lnd if you want to use lightning payments instead. And uncomment lnd_dir.
+#pay_method = "lnd"
+# lnd_dir is only needed if you want to copy macaroon and TLS cert locally
+#lnd_dir = "~/.lnd/"
+#lnd_rpcport = "10009"
+#lnd_macaroon = "invoice.macaroon"
+#lnd_cert = "tls.cert"
 
 # DO NOT CHANGE THIS TO TRUE UNLESS YOU WANT ALL PAYMENTS TO AUTOMATICALLY
 # BE CONSIDERED AS PAID.
